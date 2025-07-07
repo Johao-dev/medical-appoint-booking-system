a1e56f8adaeb43e0331a9eefd58cfbad14c1d34c
johaoDev <johaobravoespinoza1501@gmail.com>
2025-07-07 16:37:44 -0500

    add Schedule tests

--- src/main/java/zuzz/projects/medical_appointment_booking_system/domain/valueobject/Schedule.java
+++ src/main/java/zuzz/projects/medical_appointment_booking_system/domain/valueobject/Schedule.java
@@ -6,7 +6,46 @@ import zuzz.projects.medical_appointment_booking_system.shared.enums.DayOfWeek;
 
 public class Schedule {
 
-    private DayOfWeek dayOfWeek;
-    private LocalTime startTime;
-    private LocalTime endTime;
+    private final DayOfWeek dayOfWeek;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+
+    public Schedule(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public boolean overlapsWith(Schedule other) {
+        if (!isTheSameDay(other.getDayOfWeek())) {
+            return false;
+        }
+
+        return isBeforeMyStartTime(other.getEndTime()) &&
+            isAfterMyEndTime(other.getStartTime());
+    }
+
+    private boolean isTheSameDay(DayOfWeek otherDay) {
+        return this.dayOfWeek.equals(otherDay);
+    }
+
+    private boolean isBeforeMyStartTime(LocalTime otherEndTime) {
+        return this.startTime.isBefore(otherEndTime);
+    }
+
+    private boolean isAfterMyEndTime(LocalTime otherStartTime) {
+        return otherStartTime.isBefore(this.endTime);
+    }
 }
