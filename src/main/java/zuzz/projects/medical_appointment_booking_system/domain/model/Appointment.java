fd7e90c2eb77f865a34eb7191caccfef7198299a
johaoDev <johaobravoespinoza1501@gmail.com>
2025-07-19 12:37:19 -0500

    add TODO in Appointment

--- src/main/java/zuzz/projects/medical_appointment_booking_system/domain/model/Appointment.java
+++ src/main/java/zuzz/projects/medical_appointment_booking_system/domain/model/Appointment.java
@@ -38,6 +38,7 @@ public class Appointment {
         changeState(new Cancelled());
     }
 
+    // TODO: Añadir parametro Schedule newSchedule y validacion de tiempo de anticipacion
     public void reschedule() {
         changeState(new Pending());
     }
